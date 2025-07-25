151815c980fba50a8b4e8e894154c67c3efcd381
Rijan Dhakal <dhakalrijan01@gmail.com>
2025-07-25 17:01:42 +0545

    fix isVerified check in auth controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -101,6 +101,8 @@ export const login = async (req, res, next) => {
       throw error;
     }
 
+    if(existingUser.isVerified == false) customError("User is not verified.", 400);
+
     // checking if password is correct
     const isPasswordValid = await bcrypt.compare(
       password,
